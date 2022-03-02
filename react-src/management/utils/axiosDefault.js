d807986c97f8d36c6bb106145589129bcbd60aef
Morteza a <azetrom.a@gmail.com>
2022-03-02 15:56:42 +0330

    10.9.6

--- react-src/management/utils/axiosDefault.js
+++
@@ -1,7 +0,0 @@
-﻿import Axios from "axios";
-import $ from 'jquery';
-
-export default function setAxiosDefault() {
-     var xsrf = $('input[name=__RequestVerificationToken]').val() //+ 'a';
-    Axios.defaults.headers.common['__RequestVerificationToken'] = xsrf;
-}
