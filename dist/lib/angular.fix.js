0fa66467100d404e3302fd199a16258d5ffeee7e
Morteza a <azetrom.a@gmail.com>
2022-08-30 16:44:05 +0430

    اصلاح فرساز

---
+++ dist/lib/angular.fix.js
@@ -0,0 +1,9 @@
+
+angular.uppercase=function(text){
+	if(!text||!text.toUpperCase)return text;
+	return text.toUpperCase();
+ }
+ angular.lowercase=function(text){
+	 if(!text ||!text.toLowerCase)return text;
+	 return text.toLowerCase();
+ }
