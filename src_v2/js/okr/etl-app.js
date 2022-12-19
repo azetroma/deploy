ea3eebb1eeebe896d5ee5bea8b7de53cde27ef78
Morteza a <azetrom.a@gmail.com>
2022-12-19 13:11:47 +0330

    10.9.18

---
+++ src_v2/js/okr/etl-app.js
@@ -0,0 +1,10 @@
+﻿import appModule from './module.okr'
+ var app = angular.module('management');
+
+app.component('etlapp', {
+   template: '<etl-root class="tw-min-h-full tw-flex tw-flex-grow tw-w-full"></etl-root>'
+})
+
+
+
+
---
+++ src_v2/js/okr/okr-app.js
@@ -0,0 +1,10 @@
+﻿import appModule from './module.okr'
+//  var app = angular.module('management');
+
+// app.component('okrapp', {
+//    template: '<okrtag class="tw-min-h-full tw-flex tw-flex-grow tw-w-full"></okrtag>'
+// })
+
+
+
+
